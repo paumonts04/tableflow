5ca0d5cdd89fc6a40686fec311f36e081ffe3c83
paumonts04 <paumonts04@gmail.com>
2026-05-29 01:01:02 +0200

    Analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from 'next'
+import { Analytics } from "@vercel/analytics/next"
 import './globals.css'
 
 export const metadata: Metadata = {
@@ -17,6 +18,7 @@ export default function RootLayout({
   return (
     <html lang="es">
       <body>{children}</body>
+      <Analytics />
     </html>
   )
 }
